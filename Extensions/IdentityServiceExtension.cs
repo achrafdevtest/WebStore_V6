66ad46289de5c6ca1a44a6ff5c89504ef3965997
Devtest <achrafdevtest@gmail.com>
2022-09-28 23:38:15 +0100

    Add Identity service extensions

---
+++ Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebStore.Core.Models;
+using WebStore.DAL.Data.EF;
+
+namespace WebStore.Extensions;
+public static class IdentityServiceExtension
+{
+    public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration config)
+    {
+        var builder = services.AddIdentityCore<AppUser>();
+
+        builder = new IdentityBuilder(builder.UserType, builder.Services);
+        builder.AddEntityFrameworkStores<StoreIdentityContext>();
+        builder.AddSignInManager<SignInManager<AppUser>>();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"])),
+                    ValidIssuer = config["Token:Issuer"],
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                };
+            });
+
+        return services;
+    }
+}

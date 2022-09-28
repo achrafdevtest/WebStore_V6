5afb787436dbe751559a195d451c8ac4010f7269
Devtest <achrafdevtest@gmail.com>
2022-09-28 23:38:53 +0100

    Add identity context

---
+++ DAL/Data/EF/StoreIdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Core.Models;
+
+namespace WebStore.DAL.Data.EF;
+
+public class StoreIdentityContext : IdentityDbContext<AppUser>
+{
+    public StoreIdentityContext(DbContextOptions<StoreIdentityContext> options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}

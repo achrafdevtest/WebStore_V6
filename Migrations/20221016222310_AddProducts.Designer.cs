796400c7e5a6b59fe598991a409703e057a307fe
Devtest <achrafdevtest@gmail.com>
2022-10-16 23:30:32 +0100

    Add Products migration

--- Migrations/20221016222310_AddProducts.Designer.cs
+++ Migrations/20221016222310_AddProducts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebStore.DAL.Data.EF;
 
@@ -11,9 +12,10 @@ using WebStore.DAL.Data.EF;
 namespace WebStore.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20221016222310_AddProducts")]
+    partial class AddProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20221016222310_AddProducts.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStore.Migrations
+{
+    public partial class AddProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "P_Unite",
+                table: "PRODUCTS",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "P_Name",
+                table: "PRODUCTS",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "P_Description",
+                table: "PRODUCTS",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "P_Discount",
+                table: "PRODUCTS",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "P_PictureUrl",
+                table: "PRODUCTS",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "PRODUCTS",
+                keyColumn: "P_Id",
+                keyValue: 1,
+                column: "P_Discount",
+                value: 10m);
+
+            migrationBuilder.UpdateData(
+                table: "PRODUCTS",
+                keyColumn: "P_Id",
+                keyValue: 2,
+                column: "P_Discount",
+                value: 10m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "P_Discount",
+                table: "PRODUCTS");
+
+            migrationBuilder.DropColumn(
+                name: "P_PictureUrl",
+                table: "PRODUCTS");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "P_Unite",
+                table: "PRODUCTS",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "P_Name",
+                table: "PRODUCTS",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "P_Description",
+                table: "PRODUCTS",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
